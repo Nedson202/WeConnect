5c5678644f4b43c52a199af368580b6c8f58d64b
Samson <simeonsamson4@gmail.com>
2018-02-28 16:12:05 +0100

    Initial commit

---
+++ template/js/main.js
@@ -0,0 +1,10 @@
+// Initialize collapse button
+$('.button-collapse').sideNav({
+   closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+   draggable: true, // Choose whether you can drag to open on touch screens,
+ }
+);
+$('.parallax').parallax();
+$('.modal').modal();
+$('#textarea1').trigger('autoresize');
+$('.scrollspy').scrollSpy();
