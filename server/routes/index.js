218cf5382ecaa93fa82ca0e6d2ef8c3d11b6bd09
Samson <simeonsamson4@gmail.com>
2018-03-08 08:29:47 +0100

    Add route for business registration

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,10 @@ import Businesses from '../controller/business';
 import Reviews from '../controller/review';
 import userValidator from '../middlewares/user-validator';
 import validateBusiness from '../middlewares/business-validator';
-import users from '../model/user';
 
 
 export default (route) => {
   route.post('/api/v1/auth/signup', userValidator.userSignup, Auth.createUser);
+  route.post('/api/v1/auth/login', userValidator.userLogin, Auth.logUser);
+  route.post('/api/v1/businesses', validateBusiness.registerBusiness, Businesses.createBusiness);
 };
