3f0b7478658ac0170a1957768c8aa052bfa4814c
Samson <simeonsamson4@gmail.com>
2018-03-08 11:27:36 +0100

    Add middleware to filter business by category

--- server/middlewares/business-filterer.js
+++ server/middlewares/business-filterer.js
@@ -8,7 +8,7 @@ import businesses from '../model/business';
 class filterBusiness {
   /**
     *
-    *Filter business by location
+    *Filter business by category
     *@param {any} req - request value - handles data coming from the user
     *@param {any} res - response value - this is the response gotten after
     interaction with the Api routes
@@ -26,6 +26,37 @@ class filterBusiness {
     if (location) {
       filteredBusiness = businesses.filter(business =>
         location.toLowerCase() === business.location);
+      if (filteredBusiness.length === 0) {
+        res.status(404).json({
+          message: 'Business not found',
+          error: true
+        });
+      }
+      res.status(200).json(filteredBusiness);
+    }
+
+    next();
+  }
+  /**
+    *
+    *Filter business by category
+    *@param {any} req - request value - handles data coming from the user
+    *@param {any} res - response value - this is the response gotten after
+    interaction with the Api routes
+    *@param {any} next - next value - this is a middleware reserved keyword
+    responsible for allowing other methods or action in line to act only after this pressent
+    action has taken place
+    *@return {json} response object gotten
+    *@memberof filterBusiness
+  */
+  static filterByCategory(req, res, next) {
+    let filteredBusiness;
+
+    const { category } = req.query;
+
+    if (category) {
+      filteredBusiness = businesses.filter(business =>
+        category.toLowerCase() === business.category);
       if (filteredBusiness.length === 0) {
         res.status(404).json({
           message: 'Business not found',
