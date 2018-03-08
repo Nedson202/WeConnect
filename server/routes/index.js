f180fc1199c66c280600d49b80c345a54299bc5f
Samson <simeonsamson4@gmail.com>
2018-03-08 10:57:00 +0100

    Update get business route to allow filter by location

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,12 @@ import Businesses from '../controller/business';
 import Reviews from '../controller/review';
 import userValidator from '../middlewares/user-validator';
 import validateBusiness from '../middlewares/business-validator';
+import businessFilter from '../middlewares/business-filterer';
 
 
 export default (route) => {
   route.post('/api/v1/auth/signup', userValidator.userSignup, Auth.createUser);
   route.post('/api/v1/auth/login', userValidator.userLogin, Auth.logUser);
   route.post('/api/v1/businesses', validateBusiness.registerBusiness, Businesses.createBusiness);
-  route.get('/api/v1/businesses', Businesses.getBusiness);
+  route.get('/api/v1/businesses', businessFilter.filterByLocation, Businesses.getBusiness);
 };
