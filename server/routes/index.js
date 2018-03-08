fef0cc92eb094ede7a64c44549ec90ff878bea5b
Samson <simeonsamson4@gmail.com>
2018-03-08 16:17:27 +0100

    Add route to update business

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,15 @@ import Auth from '../auth/user';
 import Businesses from '../controller/business';
 import Reviews from '../controller/review';
 import userValidator from '../middlewares/user-validator';
-import validateBusiness from '../middlewares/business-validator';
+import businessValidator from '../middlewares/business-validator';
 import businessFilter from '../middlewares/business-filterer';
 
 
 export default (route) => {
   route.post('/api/v1/auth/signup', userValidator.userSignup, Auth.createUser);
   route.post('/api/v1/auth/login', userValidator.userLogin, Auth.logUser);
-  route.post('/api/v1/businesses', validateBusiness.registerBusiness, Businesses.createBusiness);
+  route.post('/api/v1/businesses', businessValidator.registerBusiness, Businesses.createBusiness);
   route.get('/api/v1/businesses', businessFilter.filterByLocation, businessFilter.filterByCategory, Businesses.getBusiness);
   route.get('/api/v1/businesses/:businessId', businessFilter.filterById);
+  route.put('/api/v1/businesses/:businessId', Businesses.updateBusiness);
 };
