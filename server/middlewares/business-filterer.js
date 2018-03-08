edd37e423c429360a9c6e9601025fddc5cb20a77
Samson <simeonsamson4@gmail.com>
2018-03-08 11:55:36 +0100

    Add middleware to get business by id

--- server/middlewares/business-filterer.js
+++ server/middlewares/business-filterer.js
@@ -68,6 +68,33 @@ class filterBusiness {
 
     next();
   }
+  /**
+    *
+    *Filter business by id
+    *@param {any} req - request value - handles data coming from the user
+    *@param {any} res - response value - this is the response gotten after
+    interaction with the Api routes
+    *@param {any} next - next value - this is a middleware reserved keyword
+    responsible for allowing other methods or action in line to act only after this pressent
+    action has taken place
+    *@return {json} response object gotten
+    *@memberof filterBusiness
+  */
+  static filterById(req, res, next) {
+    const businessId = parseInt(req.params.businessId, 10);
+    const filteredBusiness = businesses.filter(business => business.id === businessId)[0];
+
+    if (!filteredBusiness) {
+      res.status(404).json({
+        messsage: 'Business not found',
+        error: true
+      });
+    }
+
+    res.status(200).json(filteredBusiness);
+
+    next();
+  }
 }
 
 export default filterBusiness;
