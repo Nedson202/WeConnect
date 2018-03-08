b4f0e2bab8e22a0ceab1d3e28c3918b31d6a5103
Samson <simeonsamson4@gmail.com>
2018-03-08 21:06:52 +0100

    Add middleware to filter business before delete

--- server/middlewares/business-filterer.js
+++ server/middlewares/business-filterer.js
@@ -70,17 +70,17 @@ class filterBusiness {
   }
   /**
     *
-    *Filter business by id
+    *Middleware to check if business exists before deletion takes place in the controller
     *@param {any} req - request value - handles data coming from the user
     *@param {any} res - response value - this is the response gotten after
     interaction with the Api routes
     *@param {any} next - next value - this is a middleware reserved keyword
     responsible for allowing other methods or action in line to act only after this pressent
     action has taken place
-    *@return {json} response object gotten
+    *@return {json} response object with error
     *@memberof filterBusiness
   */
-  static filterById(req, res, next) {
+  static filterBeforeDelete(req, res, next) {
     const businessId = parseInt(req.params.businessId, 10);
     const filteredBusiness = businesses.filter(business => business.id === businessId)[0];
 
@@ -90,8 +90,6 @@ class filterBusiness {
         error: true
       });
     }
-
-    res.status(200).json(filteredBusiness);
 
     next();
   }
