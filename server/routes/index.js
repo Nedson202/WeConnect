06d296e6715497983eda7005d7f98dc1875e8484
Samson <simeonsamson4@gmail.com>
2018-03-09 10:07:57 +0100

    Add route to post a review

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ import Businesses from '../controller/business';
 import Reviews from '../controller/review';
 import userValidator from '../middlewares/user-validator';
 import businessValidator from '../middlewares/business-validator';
+import reviewValidator from '../middlewares/review-validator';
 import businessFilter from '../middlewares/business-filterer';
 
 
@@ -14,4 +15,5 @@ export default (route) => {
   route.get('/api/v1/businesses/:businessId', Businesses.getOneBusiness);
   route.put('/api/v1/businesses/:businessId', Businesses.updateBusiness);
   route.delete('/api/v1/businesses/:businessId', businessFilter.filterBeforeDelete, Businesses.deleteBusiness);
+  route.post('/api/v1/businesses/:businessId/reviews/', reviewValidator.reviews, Reviews.postReview);
 };
