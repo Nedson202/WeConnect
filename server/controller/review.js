0dfb354e0528ed6e19dc58f1d1522c18633e0ead
Samson <simeonsamson4@gmail.com>
2018-03-09 21:03:03 +0100

    Add controller to get all reviews under a business

--- server/controller/review.js
+++ server/controller/review.js
@@ -46,6 +46,35 @@ class Reviews {
       error: false
     });
   }
+  /**
+    *
+    *gets all reviews under a business
+    *@param {any} req - request value - handles data coming from the user
+    *@param {any} res - response value - this is the response gotten after
+    interaction with the Api routes
+    *@return {status} response object gotten
+    *@memberof Businesses
+  */
+  static getReview(req, res) {
+    const businessId = parseInt(req.params.businessId, 10);
+    const filteredBusiness = businesses.filter(business => business.id === businessId)[0];
+
+    if (!filteredBusiness) {
+      res.status(404).send({
+        message: 'Business not found, no review gotten',
+        error: true
+      });
+    }
+
+    if (filteredBusiness.reviews === undefined) {
+      res.status(404).json({
+        message: 'No review found',
+        error: true
+      });
+    }
+
+    return res.status(200).json(filteredBusiness.reviews);
+  }
 }
 
 export default Reviews;
