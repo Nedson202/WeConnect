9f8f161f9e3ce8047fca17b81cb689115d4a9802
Samson <simeonsamson4@gmail.com>
2018-03-12 15:24:37 +0100

    Refactor code to remove duplication

--- server/middlewares/business-filterer.js
+++ server/middlewares/business-filterer.js
@@ -1,98 +1,60 @@
 import businesses from '../model/business';
+
 /**
   *
-  *Business filterer
-  *@class
-  *
+  *Filter function to filter businesses by location or category
+  *If any of location or category is provided as query, the filtering takes place
+  *@param {any} req - request value - handles data coming from the user
+  *@param {any} res - response value - this is the response gotten after
+  interaction with the Api routes
+  *@param {any} next - next value - this is a middleware reserved keyword
+  responsible for allowing other methods or action in line to act only after this pressent
+  action has taken place
+  *@return {json} response object gotten
 */
-class filterBusiness {
-  /**
-    *
-    *Filter business by category
-    *@param {any} req - request value - handles data coming from the user
-    *@param {any} res - response value - this is the response gotten after
-    interaction with the Api routes
-    *@param {any} next - next value - this is a middleware reserved keyword
-    responsible for allowing other methods or action in line to act only after this pressent
-    action has taken place
-    *@return {json} response object gotten
-    *@memberof filterBusiness
-  */
-  static filterByLocation(req, res, next) {
-    let filteredBusiness;
+export function sortQuery(req, res, next) {
+  const { location, category } = req.query;
 
-    const { location } = req.query;
-
-    if (location) {
-      filteredBusiness = businesses.filter(business =>
-        location.toLowerCase() === business.location);
-      if (filteredBusiness.length === 0) {
-        res.status(404).json({
-          message: 'Business not found',
-          error: true
-        });
+  if (location || category) {
+    businesses.forEach((business) => {
+      if (location === business.location) {
+        res.status(200).json(business);
       }
-      res.status(200).json(filteredBusiness);
-    }
-
-    next();
-  }
-  /**
-    *
-    *Filter business by category
-    *@param {any} req - request value - handles data coming from the user
-    *@param {any} res - response value - this is the response gotten after
-    interaction with the Api routes
-    *@param {any} next - next value - this is a middleware reserved keyword
-    responsible for allowing other methods or action in line to act only after this pressent
-    action has taken place
-    *@return {json} response object gotten
-    *@memberof filterBusiness
-  */
-  static filterByCategory(req, res, next) {
-    let filteredBusiness;
-
-    const { category } = req.query;
-
-    if (category) {
-      filteredBusiness = businesses.filter(business =>
-        category.toLowerCase() === business.category);
-      if (filteredBusiness.length === 0) {
-        res.status(404).json({
-          message: 'Business not found',
-          error: true
-        });
+      if (category === business.category) {
+        res.status(200).json(business);
       }
-      res.status(200).json(filteredBusiness);
-    }
-
-    next();
+    });
+    res.status(404).json({
+      message: 'Business not found',
+      error: true
+    });
   }
-  /**
-    *
-    *Middleware to check if business exists before deletion takes place in the controller
-    *@param {any} req - request value - handles data coming from the user
-    *@param {any} res - response value - this is the response gotten after
-    interaction with the Api routes
-    *@param {any} next - next value - this is a middleware reserved keyword
-    responsible for allowing other methods or action in line to act only after this pressent
-    action has taken place
-    *@return {json} response object with error
-    *@memberof filterBusiness
-  */
-  static filterBeforeDelete(req, res, next) {
-    const businessId = parseInt(req.params.businessId, 10);
-    const filteredBusiness = businesses.filter(business => business.id === businessId)[0];
 
-    if (!filteredBusiness) {
-      res.status(404).json({
-        messsage: 'Business not found',
-        error: true
-      });
-    }
-
-    next();
-  }
+  next();
 }
 
-export default filterBusiness;
+/**
+  *
+  *Middleware to check if business exists before deletion takes place in the controller
+  *@param {any} req - request value - handles data coming from the user
+  *@param {any} res - response value - this is the response gotten after
+  interaction with the Api routes
+  *@param {any} next - next value - this is a middleware reserved keyword
+  responsible for allowing other methods or action in line to act only after this pressent
+  action has taken place
+  *@return {json} response object with error
+  *@memberof filterBusiness
+*/
+export function filterToDelete(req, res, next) {
+  const businessId = parseInt(req.params.businessId, 10);
+  const filteredBusiness = businesses.filter(business => business.id === businessId)[0];
+
+  if (!filteredBusiness) {
+    res.status(404).json({
+      messsage: 'Business not found',
+      error: true
+    });
+  }
+
+  next();
+}
