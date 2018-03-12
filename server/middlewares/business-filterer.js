5c12807b18badd89c57b09754129a23a0449df14
Samson <simeonsamson4@gmail.com>
2018-03-12 19:29:34 +0100

    Fix only a business displaying after querying

--- server/middlewares/business-filterer.js
+++ server/middlewares/business-filterer.js
@@ -16,14 +16,17 @@ export function sortQuery(req, res, next) {
   const { location, category } = req.query;
 
   if (location || category) {
+    const array = [];
     businesses.forEach((business) => {
       if (location === business.location) {
-        res.status(200).json(business);
+        array.push(business)
       }
       if (category === business.category) {
-        res.status(200).json(business);
+        array.push(business)
       }
     });
+
+    if (array.length > 0) {res.status(200).send(array)};
     res.status(404).json({
       message: 'Business not found',
       error: true
