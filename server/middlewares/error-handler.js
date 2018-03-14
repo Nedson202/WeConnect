46c38e9fcbfb8ab48dfbc88f62e3d8d2a3b8b920
Samson <simeonsamson4@gmail.com>
2018-03-14 09:53:42 +0100

    Fix middleware next function

--- server/middlewares/error-handler.js
+++ server/middlewares/error-handler.js
@@ -2,15 +2,13 @@ const errorHandler = (req, res, next) => {
   const errors = req.validationErrors();
 
   if (errors) {
-    res.status(400).json({
+    return res.status(400).json({
       message: errors[0].msg,
       error: true
     });
   }
 
-  if (!errors) {
-    next();
-  }
+  next();
 };
 
 export default errorHandler;
