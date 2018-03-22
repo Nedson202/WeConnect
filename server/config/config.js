a8ef0350547318b12d19506397ac5f0985aec4c2
Samson <simeonsamson4@gmail.com>
2018-03-22 22:27:12 +0100

    Update database configuration

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 
 module.exports = {
-  // development: {
-  //   username: process.env.DB_USERNAME,
-  //   password: process.env.DB_PASSWORD,
-  //   database: "weconnect-api",
-  //   host: "127.0.0.1",
-  //   port: "5432",
-  //   dialect: "postgres"
-  // },
   development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "weconnect-api",
+    host: "127.0.0.1",
+    port: "5432",
+    dialect: "postgres"
+  },
+  test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: "weconnect-api-test",
