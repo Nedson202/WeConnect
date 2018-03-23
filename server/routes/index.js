0352d0eadf5d16a92b08a581022527d93da936ea
Samson <simeonsamson4@gmail.com>
2018-03-23 11:28:32 +0100

    Add middleware for update

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,8 +11,8 @@ export default (route) => {
   route.post('/api/v1/businesses', validator.registerBusiness, errorHandler, Businesses.createBusiness);
   route.get('/api/v1/businesses', sorter.sortQuery, Businesses.getBusiness);
   route.get('/api/v1/businesses/:businessId', Businesses.getOneBusiness);
-  route.put('/api/v1/businesses/:businessId', Businesses.updateBusiness);
-  route.delete('/api/v1/businesses/:businessId', Businesses.deleteBusiness);
+  route.put('/api/v1/businesses/:businessId', sorter.filterBusiness, Businesses.updateBusiness);
+  route.delete('/api/v1/businesses/:businessId', sorter.filterBusiness, Businesses.deleteBusiness);
   route.post('/api/v1/businesses/:businessId/reviews/', validator.reviews, errorHandler, Reviews.postReview);
   route.get('/api/v1/businesses/:businessId/reviews/', Reviews.getReview);
 };
