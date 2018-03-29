686a3678f10f189808db51dd136343136c0e8a78
Samson <simeonsamson4@gmail.com>
2018-03-29 06:31:31 +0100

    Add token verification as middleware

--- server/routes/business.js
+++ server/routes/business.js
@@ -1,15 +1,13 @@
 import Businesses from '../controller/business';
-import Reviews from '../controller/review';
 import validator from '../middlewares/validator';
 import sorter from '../middlewares/business-filterer';
 import errorHandler from '../middlewares/error-handler';
+import checkAuth from '../middlewares/check-auth';
 
 export default (route) => {
-  route.post('/api/v1/businesses', validator.registerBusiness, errorHandler, Businesses.createBusiness);
+  route.post('/api/v1/businesses', checkAuth, validator.registerBusiness, errorHandler, Businesses.createBusiness);
   route.get('/api/v1/businesses', sorter.sortQuery, Businesses.getBusiness);
   route.get('/api/v1/businesses/:businessId', Businesses.getOneBusiness);
-  route.put('/api/v1/businesses/:businessId', sorter.filterBusiness, Businesses.updateBusiness);
-  route.delete('/api/v1/businesses/:businessId', sorter.filterBusiness, Businesses.deleteBusiness);
-  route.post('/api/v1/businesses/:businessId/reviews/', validator.reviews, errorHandler, Reviews.postReview);
-  route.get('/api/v1/businesses/:businessId/reviews/', Reviews.getReview);
+  route.put('/api/v1/businesses/:businessId', checkAuth, sorter.checkBusiness, Businesses.updateBusiness);
+  route.delete('/api/v1/businesses/:businessId', checkAuth, sorter.checkBusiness, Businesses.deleteBusiness);
 };
