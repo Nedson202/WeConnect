f6dcd9c25754ca2ca93ab11c0c06b8f099f5977e
Samson <simeonsamson4@gmail.com>
2018-03-29 15:35:07 +0100

    Add method to get and delete users

--- server/controller/Auth.js
+++ server/controller/Auth.js
@@ -17,7 +17,11 @@ class Auth {
       *@memberof Auth
     */
   static createUser(req, res) {
-    const { username, email, password } = req.body;
+    let { username, email, password } = req.body;
+
+    if(typeof(password) === 'number') {
+      password = password.toString();
+    }
 
     Users.create({
       username: username.toLowerCase(),
@@ -29,10 +33,27 @@ class Auth {
         user.update({
           password: passwordHash
         });
+
+        const token = jwt.sign(
+          {
+            userId: user.id,
+            username,
+            email: user.email
+          },
+          config.secretkey,
+          {
+            expiresIn: '10h'
+          }
+        );
+
         return res.status(201).json({
           message: 'Signup successful',
           error: false,
-          user
+          user: {
+            username: user.username,
+            email: user.email
+          },
+          token: token
         });
       }).catch(error => res.status(409).json({
         message: error.message,
@@ -48,7 +69,11 @@ class Auth {
       *@memberof Auth
     */
   static logUser(req, res) {
-    const { username } = req.body;
+    let { username, password } = req.body;
+
+    if(typeof(password) === 'number') {
+      password = password.toString();
+    }
 
     Users.findOne({
       where: {
@@ -56,7 +81,7 @@ class Auth {
       }
     })
       .then((user) => {
-        if (!(user && bcrypt.compareSync((req.body.password).toLowerCase(), user.password))) {
+        if (!(user && bcrypt.compareSync((password).toLowerCase(), user.password))) {
           return res.status(401).json({
             message: 'Unathorised, check your username or password',
             error: true
@@ -100,7 +125,7 @@ class Auth {
           });
         }
 
-        if (req.decoded.username !== 'admin' && req.decoded.email !== 'admin@aladmin.com') {
+        if (req.decoded.username !== 'aladmin' && req.decoded.email !== 'admin@aladmin.com') {
           return res.status(403).json({
             message: 'Forbidden, you do not have access to view all users',
             error: true
@@ -112,6 +137,25 @@ class Auth {
           error: false
         });
       });
+  }
+  /**
+      *
+      *@param {any} req - request value
+      *@param {any} res - response value
+      *@return {json} response object goten
+      *@memberof Auth
+    */
+  static deleteUser(req, res) {
+    const userId = parseInt(req.params.userId, 10);
+    return Users.destroy({
+      where: {
+        id: userId
+      }
+    })
+      .then(() => res.status(200).json({
+        message: 'User deleted successfully',
+        error: false
+      }));
   }
 }
 
