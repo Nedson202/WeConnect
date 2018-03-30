ec3175b7bc0b1c7d652aef0516a68467ebecc0c0
Samson <simeonsamson4@gmail.com>
2018-03-30 19:07:13 +0100

    Clean with eslint --fix

--- server/config/config.js
+++ server/config/config.js
@@ -4,21 +4,21 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: "weconnect-api",
-    host: "127.0.0.1",
-    port: "5432",
-    dialect: "postgres"
+    database: 'weconnect-api',
+    host: '127.0.0.1',
+    port: '5432',
+    dialect: 'postgres'
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: "weconnect_api_test",
-    host: "127.0.0.1",
-    port: "5432",
-    dialect: "postgres"
+    database: 'weconnect_api_test',
+    host: '127.0.0.1',
+    port: '5432',
+    dialect: 'postgres'
   },
   production: {
     use_env_variable: 'DATABASE_URL'
   },
   secretkey: process.env.JWT_SECRET
-}
+};
