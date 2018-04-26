228f192775b5a28113ce8ff515b4c44b437a9660
Samson <simeonsamson4@gmail.com>
2018-04-26 10:45:18 +0100

    Add array for frontend select

---
+++ client/select-options/location.js
@@ -0,0 +1,138 @@
+export const locations = [
+  {
+      "id": 1,
+      "location": "Abia"
+  },
+  {
+      "id": 2,
+      "location": "Adamawa"
+  },
+  {
+      "id": 3,
+      "location": "Akwa Ibom"
+  },
+  {
+      "id": 4,
+      "location": "Anambra"
+  },
+  {
+      "id": 5,
+      "location": "Bauchi"
+  },
+  {
+      "id": 6,
+      "location": "Bayelsa"
+  },
+  {
+      "id": 7,
+      "location": "Benue"
+  },
+  {
+      "id": 8,
+      "location": "Borno"
+  },
+  {
+      "id": 9,
+      "location": "Cross River"
+  },
+  {
+      "id": 10,
+      "location": 'Ebonyi'
+  },
+  {
+      "id": 11,
+      "location": 'Edo'
+  },
+  {
+      "id": 12,
+      "location": 'Ekiti'
+  },
+  {
+      "id": 13,
+      "location": 'Enugu'
+  },
+  {
+      "id": 14,
+      "location": 'FCT'
+  },
+  {
+      "id": 15,
+      "location": 'Gombe'
+  },
+  {
+      "id": 16,
+      "location": 'Imo'
+  },
+  {
+      "id": 17,
+      "location": 'Jigawa'
+  },
+  {
+      "id": 18,
+      "location": 'Kano'
+  },
+  {
+      "id": 19,
+      "location": 'Katsina'
+  },
+  {
+      "id": 20,
+      "location": 'Kebbi'
+  },
+  {
+      "id": 21,
+      "location": 'Kogi'
+  },
+  {
+      "id": 22,
+      "location": 'Kwara'
+  },
+  {
+      "id": 23,
+      "location": 'Lagos'
+  },
+  {
+      "id": 24,
+      "location": 'Nasarawa'
+  },
+  {
+      "id": 25,
+      "location": 'Niger'
+  },
+  {
+      "id": 26,
+      "location": 'Ogun'
+  },
+  {
+      "id": 27,
+      "location": 'Ondo'
+  },
+  {
+      "id": 28,
+      "location": 'Osun'
+  },
+  {
+      "id": 29,
+      "location": 'Plateau'
+  },
+  {
+      "id": 30,
+      "location": 'Rivers'
+  },
+  {
+      "id": 31,
+      "location": 'Sokoto'
+  },
+  {
+      "id": 32,
+      "location": 'Taraba'
+  },
+  {
+      "id": 33,
+      "location": 'Yobe'
+  },
+  {
+      "id": 34,
+      "location": 'Zamfara'
+  }
+];
