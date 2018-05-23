dcfb7d2187439bacd12fd3a71cc0295116ff1180
Negedu Samson <simeonsamson4@gmail.com>
2018-05-24 00:06:06 +0100

    update action for search feature (#22)

--- client/actions/businessQueryAction.js
+++ client/actions/businessQueryAction.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
 
-export function filterBusiness(option, query) {
-  return dispatch => axios.get(`http://localhost:4000/api/v1/businesses?${option}=${query}`).then((res) => {
+/**
+ * @description function to dispatch an action to filter businesses
+ * 
+ * @param {Oject} option
+ * 
+ * @param {Object} query
+ * 
+ * @return {Object} action dispatched by the action creator
+ */
+const filterBusiness = (option, query) => (dispatch) => 
+  axios.get(`/api/v1/businesses?${option}=${query}`).then((res) => {
     const businessList = JSON.stringify(res.data.businesses);
     sessionStorage.setItem('businessList', businessList);
-  }).catch((error) => {
+  }).catch(() => {
     sessionStorage.setItem('businessList', null);
     return 'Business not found';
   });
-}
+
+
+export default filterBusiness;
--- client/components/search/SearchResultPage.jsx
+++ client/components/search/SearchResultPage.jsx
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import Navbar from '../Navbar.jsx';
-import BusinessList from '../BusinessList.jsx';
-import Footer from '../Footer.jsx';
-import '../../index.css';
+import BusinessList from '../BusinessList';
+import '../../index.scss';
 
+/**
+ * @class SearcResultPage
+ * 
+ * @extends {Component}
+ */
 class SearchResultPage extends Component {
+  /**
+   * @description set page title
+   * 
+   * @returns {undefined}
+   * 
+   * @memberof SearchResultPage
+   */
   componentDidMount() {
     document.title = 'Search result'
   }
 
+  /**
+   * @description Renders the component to the dom
+   * 
+   * @returns {object} JSX object
+   * 
+   * @memberof SearchResultPage
+   */
   render() {
     const businessList = JSON.parse(sessionStorage.getItem('businessList'));
     const queryData = JSON.parse(sessionStorage.getItem('query'));
@@ -18,71 +33,20 @@ class SearchResultPage extends Component {
     const noBusiness = (
       <h1 className="text-center textcase" id="no-business-found">No business found with {queryData.option}: {queryData.query}</h1>
     )
+
+    const foundBusiness = (
+      <h1 className="text-center textcase" id="business-found">Displaying business with {queryData.option}: {queryData.query}</h1>
+    )
     return (
       <div>
-        <Navbar />
-
-        <div className="text-center">
-        </div>
+        <div className="text-center" />
+        { !businessList ? null : foundBusiness}
         { !businessList ? noBusiness : (
-          <div className="container card-container">
-            <h2 className="text-center textcase search-result-title">Displaying result for businesses with {queryData.option}: {queryData.query}</h2>
-            <div className="row">
-              {businessList.map(business => <div className="col-sm-12 col-md-6 col-lg-4" key={business.id}>
-                  <div class="card business-card">
-                    <div class="card-header text-center bg-transparent">
-                      <h5 className="card-title textcase">{business.name}<span>
-                        <Link to={`profile/${business.id}`} className="link"><i className="fa fa-angle-double-right float-right"></i>
-                        </Link></span></h5>
-                    </div>
-                    <div className="card-body">
-                      <div className="card-style">
-                        <p><i class="fa fa-map-marker"></i><span className="card-info address">{business.address}</span></p>
-                        <p><i class="fa fa-location-arrow"></i><span className="card-info location">{business.location}</span></p>
-                        <p><i class="fa fa-envelope"></i><span className="card-info">{business.email}</span></p>
-                      </div>
-                    </div>
-                    <div className="card-footer text-center bg-transparent">
-                      <ul className="list-unstyled list-inline">
-                          <li className="list-inline-item">
-                              <a className="btn-floating btn-sm btn-tw mx-1">
-                                <span className="fa-stack fa-lg">
-                                  <i className="fa fa-circle fa-stack-2x"></i>
-                                  <i className="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                                </span>
-                              </a>
-                          </li>
-                          <li className="list-inline-item">
-                              <a className="btn-floating btn-sm btn-li mx-1">
-                                <span className="fa-stack fa-lg">
-                                  <i className="fa fa-circle fa-stack-2x"></i>
-                                  <i className="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                                </span>
-                              </a>
-                          </li>
-                          <li className="list-inline-item">
-                              <a className="btn-floating btn-sm btn-li mx-1">
-                                <span className="fa-stack fa-lg">
-                                  <i className="fa fa-circle fa-stack-2x"></i>
-                                  <i className="fa fa-linkedin fa-stack-1x fa-inverse"></i>
-                                </span>
-                              </a>
-                          </li>
-                      </ul>
-                    </div>
-                  </div>
-              </div>)}
-            </div>
-          </div>
+          <BusinessList businesses={businessList} />
         )}
-        <Footer />
       </div>
     );
   }
 }
 
 export default SearchResultPage;
-
-// <Link to={`profile/${business.id}`} ><img className="card-img-top" src={cardImage} alt="Card image cap"/></Link>
-
-  // <h5 className="text-center card-title card-name">{business.name}</h5>
