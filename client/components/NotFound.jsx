4a53fddbd07a48ced40ae317628dd3f41800086c
Nedson202 <simeonsamson4@gmail..com>
2018-05-24 11:15:32 +0100

    Add handler for unavailable routes

---
+++ client/components/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <div className="text-center" id="no-business-found">
+        <h1>404!</h1>
+        <h1>Sorry page not found</h1>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
