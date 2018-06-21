be556e658d6ecca48efa7a4c332701ebd9c92a76
Nedson202 <simeonsamson4@gmail..com>
2018-06-21 09:53:05 +0100

    Add functionality to update user profile"

--- client/actions/imageUpload.js
+++ client/actions/imageUpload.js
@@ -1,5 +1,16 @@
 import axios from 'axios';
+import { ADD_IMAGE } from './types';
 
-const imageUploader = (userId, image) => dispatch => axios.put(`/api/v1/user/${userId}`, image); //eslint-disable-line no-unused-vars
+const setImage = (image) => {
+    return {
+        type: ADD_IMAGE,
+        image
+    }
+}
+
+const imageUploader = (userId, image) => dispatch => {
+    return axios.put(`/api/v1/user/${userId}`, image).then(
+        (res) => dispatch(setImage(res.data.user.image)))
+};//eslint-disable-line no-unused-vars
 
 export default imageUploader;
---
+++ client/components/user/userProfile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+import ImageUploader from '../ImageUpload'
+import '../../index.scss';
+
+const UserProfile = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <div className="container">
+        <div className="row col-lg-10 offset-lg-1">
+          <div className="col-lg-2">
+            <ImageUploader />
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default UserProfile;
