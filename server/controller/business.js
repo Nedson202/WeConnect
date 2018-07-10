5860a096c8c97a469646e6f71ece3dc33f926e0d
Nedson202 <simeonsamson4@gmail..com>
2018-07-10 13:49:54 +0100

    Add method to add business image

--- server/controller/business.js
+++ server/controller/business.js
@@ -1,22 +1,19 @@
-import models from '../models/index';
+import models from '../models';
 import errorMessage from '../middlewares/error-message';
+import config from '../config/config';
 
 const Businesses = models.Business;
-const Users = models.User;
 const Reviews = models.Review;
 /**
-
  *@class
- *
  */
 class BusinessMethods {
   /**
-      *
-      *@param {any} req - request value
-      *@param {any} res - response value
-      *@return {json} response object gotten
-      *@memberof BusinessMethods
-    */
+    *@param {any} req - request value
+    *@param {any} res - response value
+    *@return {json} response object gotten
+    *@memberof BusinessMethods
+  */
   static createBusiness(req, res) {
     const {
       name,
@@ -26,16 +23,15 @@ class BusinessMethods {
       category,
       description
     } = req.body;
-
-    // const conflict = [];
-
-    if (req.decoded.username === 'admin') {
+    const { username, userId } = req.decoded;
+    // check if user is an admin
+    if (username === config.admin) {
       return res.status(403).json({
         message: 'An admin can not register a business',
         error: true
       });
     }
-
+    // create business
     return Businesses.create({
       name: name.toLowerCase(),
       email: email.toLowerCase(),
@@ -43,13 +39,15 @@ class BusinessMethods {
       location: location.toLowerCase(),
       category: category.toLowerCase(),
       description: description.toLowerCase(),
-      userId: req.decoded.userId
+      userId
     })
+    // return success message and details of business created
       .then(business => res.status(201).json({
         message: 'Business registration successful',
         error: false,
         business
       }))
+      // catch error caused by duplicate registration
       .catch(error => res.status(409).json([error.errors[0].message]));
   }
   /**
@@ -63,27 +61,22 @@ class BusinessMethods {
     const limit = 6;
     let page = req.query.page || 1;
     const offset = limit * (page - 1);
-
     page = parseInt(page, 10);
-
+    // find all businesses and display them in batches using ?page={number}
     return Businesses
       .findAndCountAll({
         limit,
         offset,
-        include: [{
-          model: Users,
-          as: 'businessOwner',
-          attributes: ['username']
-        }]
       })
       .then((businesses) => {
-        if(businesses.rows.length === 0) {
+        // return empty array if no business is found
+        if (businesses.rows.length === 0) {
           return res.status(200).json({ businesses: businesses.rows });
         }
-
         const { count } = businesses;
+        // generate the total number of pages that the business found will span
         const totalPages = Math.ceil(count / limit);
-
+        // return business data and information regarding pagination of result
         return res.status(200).json({
           allData: {
             businesses: businesses.rows,
@@ -99,15 +92,15 @@ class BusinessMethods {
       });
   }
   /**
-          *
-          *@param {any} req - request value
-          *@param {any} res - response value
-          *@return {json} response object gotten
-          *@memberof BusinessMethods
-        */
+    *
+    *@param {any} req - request value
+    *@param {any} res - response value
+    *@return {json} response object gotten
+    *@memberof BusinessMethods
+  */
   static getOneBusiness(req, res) {
-    // return Businesses.findById(req.params.businessId)
     const { businessId } = req.params;
+    // return a single business. This search is done using the business id
     return Businesses.findOne({
       where: {
         id: businessId
@@ -119,20 +112,16 @@ class BusinessMethods {
       }]
     })
       .then((business) => {
+        // return message if no business is found. errorMessage is a reusable function
         if (business === null) {
           return errorMessage(res);
         }
-
-        let ratings = 0;
-        const noOfRaters = business.businessReviews.length;
-        // console.log(business.Rev);
-        business.businessReviews.map(rating => ratings += rating.rating);
-        // business.businessReviews.map(rating => ({
-        //   ratings += rating.rating;
-        // }));
-
-        const averageRating = ratings / noOfRaters;
-        // business.rating = aveRating
+        const { businessReviews } = business;
+        // count the ratings in the reviews table
+        const lengthOfRating = business.businessReviews.length;
+        // generate the average of all rating
+        const averageRating = businessReviews.reduce((totalRating, value) => totalRating + value.rating, 0) / lengthOfRating; // eslint-disable-line max-len
+        // append average rating to business found
         const oneBusiness = {
           id: business.id,
           name: business.name,
@@ -145,22 +134,21 @@ class BusinessMethods {
           createdAt: business.createdAt,
           updatedAt: business.updatedAt,
           userId: business.userId,
-          averageRating: averageRating || 0
+          averageRating: averageRating || 0,
         };
-
-
+        // return the data for business found
         return res.status(200).json({
-          business: [oneBusiness],
+          business: oneBusiness,
           error: false
         });
       })
+      // catch error which occurs on the server
       .catch(error => res.status(500).json({
         message: error.message,
         error: true
       }));
   }
   /**
-    *
     *@param {any} req - request value
     *@param {any} res - response value
     *@return {json} response object gotten
@@ -170,10 +158,9 @@ class BusinessMethods {
     const limit = 6;
     let page = req.query.page || 1;
     const offset = limit * (page - 1);
-
     page = parseInt(page, 10);
-
     const { userId } = req.params;
+    // find all business owned by a user
     return Businesses.findAndCountAll({
       where: {
         userId: parseInt(userId, 10)
@@ -182,13 +169,14 @@ class BusinessMethods {
       offset,
     })
       .then((businesses) => {
+        // return empty array if no business is found
         if (businesses.rows.length === 0) {
           return res.status(200).json({ businesses: businesses.rows });
         }
-
         const { count } = businesses;
+        // generate the total number of pages that the business found will span
         const totalPages = Math.ceil(count / limit);
-
+        // return business data and information regarding pagination of result
         return res.status(200).json({
           allData: {
             businesses: businesses.rows,
@@ -202,56 +190,41 @@ class BusinessMethods {
           }
         });
       })
-      .catch(error => {
-        return res.status(500).json({
-          message: error.message,
-          error: true
-      })});
+      // catch error which occurs on the server
+      .catch(error => res.status(500).json({
+        message: error.message,
+        error: true
+      }));
   }
   /**
-    *
     *@param {any} req - request value
     *@param {any} res - response value
     *@return {json} response object gotten
     *@memberof BusinessMethods
   */
   static updateBusiness(req, res) {
-    // const conflict = [];
-    const { business } = req
-
-    // const businessId = parseInt(req.params.businessId, 10);
-    // return Businesses.findById(businessId)
-    //   .then((business) => {
-    //     if (business.userId !== req.decoded.userId) {
-    //       return res.status(403).json({
-    //         message: 'Forbidden, you do not have access to modify this business',
-    //         error: true
-    //       });
-    //     }
-
-        business.update({
-          name: req.body.name || business.name,
-          email: req.body.email || business.email,
-          address: req.body.address || business.address,
-          location: req.body.location || business.location,
-          category: req.body.category || business.category,
-          description: req.body.description || business.description,
-          image: req.body.image || business.image,
-        })
-          .then(() => res.status(200).json({
-            message: 'Business updated successfully',
-            error: false,
-            business
-          }))
-          // .catch((error) => {
-          //   if (error.errors[0].path === 'name') {
-          //     conflict.push('Business with name is already registered');
-          //   }
-
-          //   return res.status(409).json(conflict);
-          // });
-          .catch(error => res.status(409).json([error.errors[0].message]));
-      // });
+    const { business } = req;
+    const {
+      name, email, address, location, category, description, image
+    } = req.body;
+    // upddate business information
+    business.update({
+      name: name || business.name,
+      email: email || business.email,
+      address: address || business.address,
+      location: location || business.location,
+      category: category || business.category,
+      description: description || business.description,
+      image: image || business.image,
+    })
+      // return success message and updated business information
+      .then(() => res.status(200).json({
+        message: 'Business updated successfully',
+        error: false,
+        business
+      }))
+      // catch error which occurs on the server
+      .catch(error => res.status(409).json([error.errors[0].message]));
   }
   /**
     *
@@ -261,24 +234,19 @@ class BusinessMethods {
     *@memberof BusinessMethods
   */
   static updateBusinessImage(req, res) {
-    // const businessId = parseInt(req.params.businessId, 10);
-    const { business } = req
-
-    // return Businesses.findById(businessId)
-    //   .then((business) => {
-        business.update({
-          image: req.body.image || business.image,
-        })
-          .then(() => {
-            return res.status(200).json({
-            message: 'Image uploaded successfully',
-            error: false,
-            business
-          })})
-      // });
+    const { business } = req;
+    // upddate business image
+    business.update({
+      image: req.body.image || business.image,
+    })
+    // return success message and updated business information
+      .then(() => res.status(200).json({
+        message: 'Image uploaded successfully',
+        error: false,
+        business
+      }));
   }
   /**
-    *
     *@param {any} req - request value
     *@param {any} res - response value
     *@return {status} response object gotten
@@ -286,17 +254,21 @@ class BusinessMethods {
   */
   static deleteBusiness(req, res) {
     const businessId = parseInt(req.params.businessId, 10);
+    const { username, userId } = req.decoded;
+    // find a business by its id
     return Businesses.findOne({
       where: {
         id: businessId
       }
     }).then((business) => {
-      if (req.decoded.username === 'admin' || business.userId === req.decoded.userId) {
+      // delete business if it is an admin or the business owner
+      if (username === config.admin || business.userId === userId) {
         return Businesses.destroy({
           where: {
             id: businessId
           }
         })
+          // return success message for business deleted
           .then(() => res.status(200).json({
             message: 'Business deleted successfully',
             error: false
