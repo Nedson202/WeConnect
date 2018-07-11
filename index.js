0d7aa6430845e536c57c4d7fa285189d7900bdb5
Nedson202 <simeonsamson4@gmail..com>
2018-07-11 17:47:52 +0100

    Add heroku setup file

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use(express.static('dist'));
 // app.use(express.static(path.resolve(__dirname, './dist')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
