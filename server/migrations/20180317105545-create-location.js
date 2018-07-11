94d0b2025d3d734140b318fecedd39113d10fa0f
Nedson202 <simeonsamson4@gmail..com>
2018-07-11 15:33:28 +0100

    Update migration for locations and categories table

---
+++ server/migrations/20180317105545-create-location.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Locations', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    location: {
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Locations') // eslint-disable-line no-unused-vars
+};
