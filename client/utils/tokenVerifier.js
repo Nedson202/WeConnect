c68689248527e48bc8890ce2edf0e0c0230e3b67
Nedson202 <simeonsamson4@gmail..com>
2018-07-11 17:36:07 +0100

    Add heroku setup file

--- client/utils/tokenVerifier.js
+++ client/utils/tokenVerifier.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-const decodeToken = token => jwt.verify(token, process.env.JWT_SECRET, (err) => {
+const decodeToken = token => jwt.verify(token, process.env.JWTSECRET, (err) => {
   if (err) return err;
 
   return false;
